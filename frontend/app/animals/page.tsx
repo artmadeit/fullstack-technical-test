b75ee37808c102de86b7add64ee07554b9e4687c
artmadeit <this.arthur.email@gmail.com>
2024-09-13 10:34:22 -0500

    add Header component

--- frontend/app/animals/page.tsx
+++ frontend/app/animals/page.tsx
@@ -1,3 +1,10 @@
+import { Header } from '@/components/Header/Header';
+
 export default function AnimalListPage() {
-  return <>Animales </>;
+  return (
+    <>
+      <Header />
+      Animales
+    </>
+  );
 }
---
+++ frontend/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import { Button, Group } from '@mantine/core';
+// import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './Header.module.css';
+
+export function Header() {
+  // const [opened, { toggle }] = useDisclosure(false);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.inner}>
+        <Group>
+          {/* <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" /> */}
+          <MantineLogo size={28} />
+        </Group>
+        <Group>
+          {/* <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+            {items}
+          </Group> */}
+          <Group visibleFrom="sm">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </Group>
+      </div>
+    </header>
+  );
+}
