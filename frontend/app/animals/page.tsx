1a435a44cb367b1cb896ee830f4c7e397fa0b6f4
artmadeit <this.arthur.email@gmail.com>
2024-09-13 15:00:33 -0500

    feat: create a SideNavBar component

--- frontend/app/animals/page.tsx
+++ frontend/app/animals/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/Header/Header';
+import { SideNavBar } from '@/components/SideNavBar/SideNavBar';
 
 export default function AnimalListPage() {
   return (
     <>
       <Header />
+      <SideNavBar />
       Animales
     </>
   );
---
+++ frontend/components/SideNavBar/SideNavBar.tsx
@@ -0,0 +1,7 @@
+export function SideNavBar() {
+  return (
+    <div>
+      <h1>NavBar</h1>
+    </div>
+  );
+}
