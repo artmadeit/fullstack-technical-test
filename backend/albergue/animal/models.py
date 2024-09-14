532021c49cf43067a2f26086622ffaf2b0e34b92
artmadeit <this.arthur.email@gmail.com>
2024-09-14 12:10:45 -0500

    feat: animal model

--- backend/albergue/animal/models.py
+++ backend/albergue/animal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+MEDIUM_LENGTH = 50
+
+class AnimalType(models.TextChoices):
+    DOG = "D"
+    CAT = "C"
+
+class AnimalStatus(models.TextChoices):
+    AWAITING_ADOPTION = "AWAITING_ADOPTION"
+    IN_ADOPTION = "IN_ADOPTION"
+    ADOPTED = "ADOPTED"
+
+class Animal(models.Model):
+    name = models.CharField(max_length=MEDIUM_LENGTH, blank=False, null=False)    
+    age = models.CharField(max_length=MEDIUM_LENGTH, blank=False, null=False)
+    type = models.CharField(max_length=1, choices=AnimalType.choices, null=False)
+    breed = models.CharField(max_length=MEDIUM_LENGTH, blank=False, null=False)
+    status = models.CharField(max_length=MEDIUM_LENGTH, choices=AnimalStatus.choices, null=False)
--- backend/albergue/settings.py
+++ backend/albergue/settings.py
@@ -34,8 +34,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = ["rest_framework"]
+LOCAL_APPS = ["albergue.animal"]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
