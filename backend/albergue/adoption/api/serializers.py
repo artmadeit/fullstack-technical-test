86d493d92429870b34db953f7c19d6ce1c479453
artmadeit <this.arthur.email@gmail.com>
2024-09-14 18:55:50 -0500

    feat: users api

---
+++ backend/albergue/adoption/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from albergue.adoption.models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # We only allow create user (no superusers)
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password'],
+            role=validated_data['role'],
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        # TODO: check email <=
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
--- backend/albergue/urls.py
+++ backend/albergue/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("animals/", include("albergue.animal.api.urls")),
+    path("adoptions/", include("albergue.adoption.api.urls")),
 ]
