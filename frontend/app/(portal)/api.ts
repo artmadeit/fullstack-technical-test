1d692bf20e79fae010cc53e3b7f916e848a4364b
artmadeit <this.arthur.email@gmail.com>
2024-09-14 21:02:14 -0500

    style: format code

--- frontend/app/(portal)/api.ts
+++ frontend/app/(portal)/api.ts
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
 
 export const api = axios.create({
-  baseURL: apiUrl
+  baseURL: apiUrl,
 });
 
-export const fetcher = (url: string) => api.get(url).then(res => res.data)
+export const fetcher = (url: string) => api.get(url).then((res) => res.data);
