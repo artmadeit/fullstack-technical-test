ebadb3bd3c8ca888eced275f59b0aac736b4cd50
artmadeit <this.arthur.email@gmail.com>
2024-09-14 20:31:10 -0500

    fix: urls bug

--- backend/albergue/adoption/api/urls.py
+++ backend/albergue/adoption/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, AdoptionViewSet
 
 router = DefaultRouter()
-router.register(r'', AdoptionViewSet)
 router.register(r'users', UserViewSet)
+router.register(r'', AdoptionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
