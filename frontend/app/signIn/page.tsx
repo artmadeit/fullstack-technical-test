8831c68aef45d8601952f07048fd341f143e9cb7
artmadeit <this.arthur.email@gmail.com>
2024-09-15 20:23:49 -0500

    feat: move sign in to its own file

--- frontend/app/signIn/page.tsx
+++ frontend/app/signIn/page.tsx
@@ -1,9 +1,60 @@
-import { Welcome } from "@/components/Welcome/Welcome";
+'use client';
+
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Center,
+  Group,
+  LoadingOverlay,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { signIn } from "next-auth/react"
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  });
+
   return (
-    <>
-      <Welcome />
-    </>
+    <Center h="100vh">
+      <form onSubmit={form.onSubmit((values) => {
+        setLoading(true);
+        setError(null);
+        signIn("credentials", {
+          email: values.email,
+          password: values.password,
+          callbackUrl: "/animals"
+        })
+      })}>
+        <Stack w="70vh">
+          <LoadingOverlay visible={loading} />
+          <TextInput
+            label="Email"
+            required
+            type="email"
+            placeholder="hello@gluesticker.com"
+            {...form.getInputProps('email')} />
+          <PasswordInput label="Contraseña" required {...form.getInputProps('password')} />
+          {error && (
+            <Text c="red" size="sm" mt="sm">
+              {error}
+            </Text>
+          )}
+          <Button type="submit">Iniciar sesión</Button>
+        </Stack>
+      </form>
+    </Center>
+
   );
 }
--- frontend/components/Welcome/Welcome.tsx
+++ frontend/components/Welcome/Welcome.tsx
@@ -1,37 +1,14 @@
-'use client';
-
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import {
+  Anchor,
   Button,
   Group,
-  LoadingOverlay,
-  Modal,
-  PasswordInput,
   Text,
-  TextInput,
   Title,
 } from '@mantine/core';
-import { useForm } from '@mantine/form';
-import { useDisclosure } from '@mantine/hooks';
 import classes from './Welcome.module.css';
-import { signIn } from "next-auth/react"
+import Link from 'next/link';
 
 export function Welcome() {
-  const [opened, { toggle, close }] = useDisclosure(false);
-  const formType = 'login';
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const router = useRouter();
-
-  const form = useForm({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-  });
-
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
@@ -44,42 +21,8 @@ export function Welcome() {
         voluntarios que trabajan en su albergue ,los adoptantes y las adopciones.
       </Text>
       <Group justify="center" mt="xl">
-        <Button onClick={toggle}>Adopte su mascota</Button>
+        <Button component={Link} href="signIn">Adopte su mascota</Button>
       </Group>
-      <Modal opened={opened} onClose={close}>
-        {formType === 'login' ? (
-          <form onSubmit={form.onSubmit((values) => {
-            setLoading(true);
-            setError(null);
-            signIn("credentials", {
-              email: values.email,
-              password: values.password,
-              callbackUrl: "/animals"
-            })
-          })}>
-            <LoadingOverlay visible={loading} />
-            <TextInput
-              label="Email"
-              required
-              type="email"
-              placeholder="hello@gluesticker.com"
-              {...form.getInputProps('email')} />
-            <PasswordInput label="Contraseña" required {...form.getInputProps('password')} />
-            {error && (
-              <Text c="red" size="sm" mt="sm">
-                {error}
-              </Text>
-            )}
-            <Group justify="space-between" mt="xl">
-              <Button type="submit">Iniciar sesión</Button>
-            </Group>
-          </form>
-        ) : (
-          <>
-            {/* TODO: can anyone create it's account? */}
-          </>
-        )}
-      </Modal>
     </>
   );
 }
